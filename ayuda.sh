62d0f664c0dc36ffbd35bfe00e68fad198f6393d
GretRincon <g.rincon@ciencias.unam.mx>
2025-04-22 22:13:49 -0600

    Archivo ayuda.sh

---
+++ ayuda.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\033[1;35m Bienvenido. Esta terminal cuenta con 8 comandos para realizar divertidas actividades \033[0m"
+echo -e " \"ayuda\" es el primer comando. Aqui te explicaremos que comandos tenemos disponibles y para que puedes usarlos"
+
+echo -e " \"infosis\": Te mostraremos la información del sistema, como la memoria RAM, la arquitectura y versión del SO"
+
+echo -e " \"fecha\": Verás la fecha y hora exacta al momento de ejecutarlo"
+
+echo -e " \"busca\": Abrirá un menu con el cual puedes concontrar un archivo dentro del directorio que indiques. Debes ingresar la carpeta y tu archivo."
+
+echo -e " \"creditos \": Verás a los grandiosos programadores que crearon esta bonita terminal"
+
+echo -e " \"jugar\": Abrirá un juego clasico y muy entretenido para cuando estes aburrido"
+#Jugemos buscaminas o gato
+
+echo -e " \"musica\": Tendrás acceso a una biblioteca musical para poder reproducir música"
+
+echo -e " \"salir\": Huye de esta terminal, si tecleas esa palabra mágica volverás a la normalidad"
