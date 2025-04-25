83875ab27a5d288a39237d40f93cab531ed1d35e
GretRincon <g.rincon@ciencias.unam.mx>
2025-04-25 00:24:49 -0600

    [Gret][ADD] Se agregaron comentarios para facilitar la interacción

--- busca.sh
+++ busca.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-echo "¿Cuál es el nombre de la carpeta donde quieres buscar?"
+echo "¿Cuál es la carpeta donde quieres buscar?"
+echo "Si solo presionas enter buscara en la actual, en caso contrario ingresa una ruta"
 read -e -p "> " carpeta
 
 echo "¿Cuál es el nombre del archivo que estas buscando?"
+echo "Puede que no recuerdes el nombre completo, con un poco bastará"
 read -e -p "> " archivo
 
+#Si la carpeta se ingreso con enter, significa que buscaremos en el directorio actual
+if [ -z "$carpeta" ]; then
+    carpeta="."
+fi
 
+#Para directorios no reconocidos mandamos un mensaje de error
 if [ ! -d "$carpeta" ]; then
     echo "La carpeta no existe"
     exit 1
-fi
+fi 
 
 echo "Buscando..... "
 #$carpeta/$archivo
